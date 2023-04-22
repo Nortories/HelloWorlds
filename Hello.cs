2a87dca8affa4a7bf14d29cfba2553ffee467284
Nortories <98294242+Nortories@users.noreply.github.com>
2023-04-21 21:57:41 -0600

    first commit

---
+++ Hello.cs
@@ -0,0 +1,8 @@
+
+string helloWorld = "hello world";
+HelloWorld(helloWorld);
+
+void HelloWorld(string helloWorld)
+{
+    System.Console.WriteLine(helloWorld);
+}
---
+++ Hello.py
@@ -0,0 +1,6 @@
+def main():
+    helloWorld = "hello world"
+    print(helloWorld)
+
+if __name__ == "__main__":
+    main()
